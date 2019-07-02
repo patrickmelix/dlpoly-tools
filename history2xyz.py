de289eb8c25cf703d91f82f1b1a892ecff02ea59
Patrick Melix <patrick.melix@uni-leipzig.de>
2019-07-02 10:35:58 +0200

    update history2xyz to be more flexible with names

--- history2xyz.py
+++ history2xyz.py
@@ -5,7 +5,9 @@
 # by Patrick Melix
 # 2017/07/08
 #
-# You can import the module and use the functions or call it as a script
+# You can import the module and use the functions or call it as a script.
+# Note that the *atomNames* are highly individual.
+# Added the guess_element function to automatically handle everything not covered by *atomNames*.
 import os, sys, argparse
 
 
@@ -41,6 +43,25 @@ def main(inFile='HISTORY', outFile='traj.xyz', samePath=False, verbose=False):
 
     convertHistory2XYZ(inFile, outFile, atomNames, verbose)
     if verbose: print('Done!')
+
+def guess_element(string):
+    """ Tries to guess chemical symbol from string.
+    If not successfull retruns 'X'.
+    """
+    symbols = ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']
+    #clean out numbers
+    tmp = ''.join([x for x in string if not x.isdigit()])
+    #capitalize
+    tmp = tmp.capitalize()
+    #reduce length to 2
+    if len(tmp) > 2:
+        tmp = tmp[:2]
+    if tmp in symbols:
+        return tmp
+    elif tmp[0] in symbols:
+        return tmp[0]
+    else:
+        return "X"
 
 def convertHistory2XYZ(inFile, outFile, atomNames, verbose=False):
     bunchsize = 1000000     # Experiment with different sizes 1000000
@@ -90,7 +111,10 @@ def convertHistory2XYZ(inFile, outFile, atomNames, verbose=False):
                 tmp = []
                 for i in range(0,levcfg+1):
                     tmp.extend([float(x) for x in r.readline().strip().split()])
-                name = atomNames[split[0].lower()]
+                if split[0].lower() in atomNames:
+                    name = atomNames[split[0].lower()]
+                else:
+                    name = guess_element(split[0].lower())
                 bunch.append(("{:4}"+("{:16f}")*nItems+'\n').format(name,*tmp))
 
             if len(bunch) == bunchsize:
